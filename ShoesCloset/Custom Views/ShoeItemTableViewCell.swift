058edb77e065e1c6830c8fc272cbcb0c5fa01438
wendy <w2699033@gmail.com>
2025-04-21 16:57:24 +0800

    add padding between cells
    add containerView to contentView and add all the elements to the containerView
    also revise layout

--- ShoesCloset/Custom Views/ShoeItemTableViewCell.swift
+++ ShoesCloset/Custom Views/ShoeItemTableViewCell.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ShoeItemTableViewCell: UITableViewCell {
     
+    private let containerView = UIView()
     let shoeCellImageView = UIImageView(frame: .zero)
     let upperTitleLabel = UILabel()
     let colorTitleLabel = UILabel()
@@ -21,10 +22,11 @@ class ShoeItemTableViewCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         backgroundColor = .clear
-        contentView.backgroundColor = .red
+        contentView.backgroundColor = .clear
         contentView.layer.cornerRadius = 12
         contentView.layer.masksToBounds = true
         
+        setUpContainer()
         configure()
     }
     
@@ -39,6 +41,24 @@ class ShoeItemTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
+    
+    func setUpContainer() {
+        
+        contentView.addSubview(containerView)
+
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        containerView.backgroundColor = .white
+        containerView.layer.cornerRadius = 12
+        
+        NSLayoutConstraint.activate([
+            containerView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            containerView.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -10),
+            containerView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 0),
+            containerView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -0),
+            containerView.heightAnchor.constraint(equalToConstant: 84)
+        ])
+    }
+    
     func setUpCell(image: UIImage, brand: String, series: String, colorway: String, count: Int) {
         shoeCellImageView.image = image
         upperTitleLabel.text = brand + " " + series
@@ -49,11 +69,11 @@ class ShoeItemTableViewCell: UITableViewCell {
 
     
     func configure() {
-        addSubview(shoeCellImageView)
-        addSubview(upperTitleLabel)
-        addSubview(colorTitleLabel)
-        addSubview(wornCountTitleLabel)
-        addSubview(wornCountLabel)
+        
+        [shoeCellImageView, upperTitleLabel, colorTitleLabel, wornCountTitleLabel, wornCountLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            containerView.addSubview($0)
+        }
 
         wornCountLabel.textAlignment = .right
         wornCountLabel.textColor = UIColor(hex: "F2771F")
@@ -73,12 +93,12 @@ class ShoeItemTableViewCell: UITableViewCell {
         wornCountLabel.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
-            shoeCellImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor),
-            shoeCellImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 23),
+            shoeCellImageView.centerYAnchor.constraint(equalTo: containerView.centerYAnchor),
+            shoeCellImageView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor, constant: 23),
             shoeCellImageView.widthAnchor.constraint(equalToConstant: 56),
             shoeCellImageView.heightAnchor.constraint(equalToConstant: 56),
             
-            upperTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 19),
+            upperTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 19),
             upperTitleLabel.leadingAnchor.constraint(equalTo: shoeCellImageView.trailingAnchor, constant: 16),
             upperTitleLabel.widthAnchor.constraint(equalToConstant: 202),
             upperTitleLabel.bottomAnchor.constraint(equalTo: colorTitleLabel.topAnchor, constant: -4),
@@ -86,15 +106,15 @@ class ShoeItemTableViewCell: UITableViewCell {
             colorTitleLabel.topAnchor.constraint(equalTo: upperTitleLabel.bottomAnchor, constant: 4),
             colorTitleLabel.leadingAnchor.constraint(equalTo: upperTitleLabel.leadingAnchor),
             colorTitleLabel.widthAnchor.constraint(equalToConstant: 202),
-            colorTitleLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -21),
+            colorTitleLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -21),
             
-            wornCountTitleLabel.topAnchor.constraint(equalTo: self.topAnchor, constant: 17.5),
+            wornCountTitleLabel.topAnchor.constraint(equalTo: containerView.topAnchor, constant: 17.5),
             wornCountTitleLabel.leadingAnchor.constraint(equalTo: upperTitleLabel.trailingAnchor),
             wornCountTitleLabel.widthAnchor.constraint(equalToConstant: 53),
             wornCountTitleLabel.heightAnchor.constraint(equalToConstant: 22),
             
             wornCountLabel.topAnchor.constraint(equalTo: wornCountTitleLabel.bottomAnchor, constant: 3),
-            wornCountLabel.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: -19),
+            wornCountLabel.bottomAnchor.constraint(equalTo: containerView.bottomAnchor, constant: -19),
             wornCountLabel.leadingAnchor.constraint(equalTo: upperTitleLabel.trailingAnchor),
             wornCountLabel.widthAnchor.constraint(equalToConstant: 53),
             wornCountLabel.heightAnchor.constraint(equalToConstant: 22),
